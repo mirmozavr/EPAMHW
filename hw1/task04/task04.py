a1f08ad929eaf84f2c59f1f5d0a2af569f5aac5e
Mo <mirmozavr@gmail.com>
2021-03-02 22:57:03 +0300

    used defaultdict

--- hw1/task04/task04.py
+++ hw1/task04/task04.py
@@ -1,26 +1,27 @@
 """
-Classic task, a kind of walnut for you
+Classic task, a kind of walnut for you.
+
 Given four lists A, B, C, D of integer values,
-    compute how many tuples (i, j, k, l) there are such that A[i] + B[j] + C[k] + D[l] is zero.
+compute how many tuples (i, j, k, l) there are such that
+A[i] + B[j] + C[k] + D[l] is zero.
 We guarantee, that all A, B, C, D have same length of N where 0 ≤ N ≤ 1000.
 """
 
+from collections import defaultdict
 
 def check_sum_of_four(a: list, b: list, c: list, d: list) -> int:
-    s = {}
-    N = len(a)
+    """Count sum of four where sum = 0"""
+    s = defaultdict(int)
+    n = len(a)
     ans = 0
-    for i in range(N):
-        for j in range(N):
+    for i in range(n):
+        for j in range(n):
             remainder = 0 - (a[i] + b[j])
-            if remainder not in s:
-                s[remainder] = 1
-            else:
-                s[remainder] += 1
+            s[remainder] += 1
 
-    for k in range(N):
-        for l in range(N):
-            subtrahend = c[k] + d[l]
+    for i in range(n):
+        for j in range(n):
+            subtrahend = c[i] + d[j]
             if subtrahend in s:
                 ans += s[subtrahend]
     return ans
