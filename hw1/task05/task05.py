2eda46803938d4345d151a0cba3cd456313da1d5
Mo <mirmozavr@gmail.com>
2021-03-02 15:33:51 +0300

    task05

---
+++ hw1/task05/task05.py
@@ -0,0 +1,28 @@
+"""
+Given a list of integers numbers "nums".
+You need to find a sub-array with length less equal to "k", with maximal sum.
+The written function should return the sum of this sub-array.
+Examples:
+    nums = [1, 3, -1, -3, 5, 3, 6, 7], k = 3
+    result = 16
+"""
+
+
+def find_maximal_subarray_sum(nums: list, k: int):
+    """Max sum of continuous sub-array"""
+    ans = -float("INF")
+    index = 0
+    N = len(nums)
+    # fixing situation where k > N
+    k = min(N, k)
+    # find max subarray sum and its start index
+    for i in range(N - k + 1):
+        if sum(nums[i : i + k]) > ans:
+            ans = sum(nums[i : i + k])
+            index = i
+
+    # exclude negative numbers
+    for number in nums[index : index + k]:
+        if number < 0:
+            ans -= number
+    return ans
---
+++ hw1/task05/test_task05.py
@@ -0,0 +1,23 @@
+import os
+
+import pytest
+from task05 import find_maximal_subarray_sum
+
+script_dir = os.path.dirname(__file__)
+
+
+@pytest.mark.parametrize(
+    ["data", "length", "expected_result"],
+    [
+        ([1, 3, -1, -3, 5, 3, 6, 7], 3, 16),
+        ([], 4, 0),
+        ([1], 2, 1),
+        ([-1, -2, -3, -4], 2, 0),
+        ([1, -2, -3, -4], 3, 1),
+        ([1, 3, -1, -3, 5, 3, 6, 7], 0, 0),
+    ],
+)
+def test_find_maximum_and_minimum(data: list, length: int, expected_result: int):
+    actual_result = find_maximal_subarray_sum(data, length)
+
+    assert actual_result == expected_result
