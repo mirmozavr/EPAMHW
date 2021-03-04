476136054b604f27f77dd52e5c5f622a1d026501
Movses <68658828+mirmozavr@users.noreply.github.com>
2021-03-04 20:56:04 +0300

    Hw1 task02 (#2)
    * task01 with tests
    
    * task02 with tests
    
    * Names list changed to tuple, file management
    
    * Minor flake error fixes
    
    * If statements refactoring, more tests
    
    * Return expression instead of true or false

---
+++ hw1/task02/task02.py
@@ -0,0 +1,22 @@
+"""
+Given a cell with "it's a fib sequence" from slideshow.
+
+please write function "check_fib", which accepts a Sequence of integers, and
+returns if the given sequence is a Fibonacci sequence
+We guarantee, that the given sequence contain >= 0 integers inside.
+"""
+
+
+def check_fib(data: list) -> bool:
+
+    # process short data sequences (2 or less)
+    if len(data) < 3:
+        return data in ([0], [0, 1])
+
+    if data[0:2] != [0, 1]:
+        return False
+    # process longer data sequences(3 or more)
+    for i in range(2, len(data)):
+        if data[i] != data[i - 1] + data[i - 2]:
+            return False
+    return True
---
+++ hw1/task02/test_task02.py
@@ -0,0 +1,48 @@
+import pytest
+from task02 import check_fib
+
+true_fib_seq = [
+    0,
+    1,
+    1,
+    2,
+    3,
+    5,
+    8,
+    13,
+    21,
+    34,
+    55,
+    89,
+    144,
+    233,
+    377,
+    610,
+    987,
+    1597,
+    2584,
+    4181,
+    6765,
+]
+
+
+@pytest.mark.parametrize(
+    ("value", "expected_result"),
+    [
+        (true_fib_seq, True),
+        ([0], True),
+        ([0, 1], True),
+        ([0, 0], False),
+        ([0, -1], False),
+        ([0, 1, 1, 2, 3, 5, 8], True),
+        ([], False),
+        ([-5, 0, 1, 1, 2], False),
+        ([1, 1, 2], False),
+        ([0, 1, 1], True),
+        ([0, -1, -1], False),
+    ],
+)
+def test_check_fib(value: list, expected_result: bool):
+    actual_result = check_fib(value)
+
+    assert actual_result == expected_result
