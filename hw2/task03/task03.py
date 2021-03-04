5b3714b18c8cd12ea10e2ea24df56482a7478464
Mo <mirmozavr@gmail.com>
2021-03-04 23:12:10 +0300

    Hw2 task03

---
+++ hw2/task03/task03.py
@@ -0,0 +1,19 @@
+"""Write a function.
+
+That takes K lists as arguments and returns all possible
+lists of K items where the first element is from the first list,
+the second is from the second and so one.
+You may assume that that every list contain at least one element
+Example:
+assert combinations([1, 2], [3, 4]) == [
+    [1, 3],
+    [1, 4],
+    [2, 3],
+    [2, 4],
+]
+"""
+import itertools
+
+
+def combinations(*args: list) -> list:
+    return list(itertools.product(*args))
---
+++ hw2/task03/test_task03.py
@@ -0,0 +1,17 @@
+import pytest
+from task03 import combinations
+
+
+@pytest.mark.parametrize(
+    ("value", "expected_result"),
+    [
+        (([1, 2], [3, 4]), [(1, 3), (1, 4), (2, 3), (2, 4)]),
+        (([1], [3]), [(1, 3)]),
+        (([5, 9], [9, 5]), [(5, 9), (5, 5), (9, 9), (9, 5)]),
+        (([0, 0], [0, 0]), [(0, 0), (0, 0), (0, 0), (0, 0)]),
+    ],
+)
+def test_check_fib(value: list, expected_result: bool):
+    actual_result = combinations(*value)
+
+    assert actual_result == expected_result
