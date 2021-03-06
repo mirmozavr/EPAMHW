b73d50d64c124c02e632f50d2091a40dac903397
Mo <mirmozavr@gmail.com>
2021-03-06 14:19:10 +0300

    Hw3_t04

---
+++ hw3/tests/test_hw3_t04.py
@@ -0,0 +1,8 @@
+from hw3.hw3_t04 import is_armstrong
+
+assert is_armstrong(153) is True, "Is Armstrong number"
+assert is_armstrong(10) is False, "Is not Armstrong number"
+assert is_armstrong(371) is True, "Is Armstrong number"
+assert is_armstrong(407) is True, "Is Armstrong number"
+assert is_armstrong(354) is False, "Is not Armstrong number"
+assert is_armstrong(6846525) is False, "Is not Armstrong number"
