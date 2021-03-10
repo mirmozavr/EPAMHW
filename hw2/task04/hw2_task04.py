d670f3e316f2f074351356d6859f9a01b3b3364d
Mo <mirmozavr@gmail.com>
2021-03-10 21:12:45 +0300

    Draft

--- hw2/task04/hw2_task04.py
+++ hw2/task04/hw2_task04.py
@@ -30,6 +30,9 @@ def cache(func: Callable) -> Callable:
     f_cache = {}
 
     def wrapper(*args: Any) -> Any:
+        print(args)
+        if isinstance(args, list):
+            args = tuple(*args)
         if args in f_cache:
             return f_cache[args]
 
@@ -37,3 +40,29 @@ def cache(func: Callable) -> Callable:
         return f_cache[args]
 
     return wrapper
+
+
+cache_function = cache(function)
+val_1 = cache_function(*[100, 200])
+val_2 = cache_function(*[100, 200])
+assert val_1 is val_2
+
+
+some = 100, 200
+cache_calc_sum = cache(calc_sum)
+val_3 = cache_calc_sum(*[1, 2])
+val_4 = cache_calc_sum(*[1, 2])
+assert val_3 is val_4
+
+
+cache_sum = cache(sum)
+v1 = cache_sum([100, 200])
+v2 = cache_sum([100, 200])
+v3 = cache_sum([100, 200])
+assert v1 is v2 is v3
+
+
+cache_sum = cache(sum)
+v5 = cache_sum([30, 50, 10, 40])
+v6 = cache_sum([30, 50, 10, 40])
+assert v5 is v6
--- hw2/task04/test_hw2_task04.py
+++ hw2/task04/test_hw2_task04.py
@@ -2,33 +2,30 @@ from hw2_task04 import cache, calc_sum, function
 
 
 def test_func():
-    some = 100, 200
     cache_function = cache(function)
-    val_1 = cache_function(*some)
-    val_2 = cache_function(*some)
+    val_1 = cache_function(*[100, 200])
+    val_2 = cache_function(*[100, 200])
     assert val_1 is val_2
 
 
 def test_calc_sum():
     some = 100, 200
     cache_calc_sum = cache(calc_sum)
-    val_3 = cache_calc_sum(*some)
-    val_4 = cache_calc_sum(*some)
+    val_3 = cache_calc_sum([100, 200])
+    val_4 = cache_calc_sum([100, 200])
     assert val_3 is val_4
 
 
 def test_sum_1():
-    some = 100, 200
     cache_sum = cache(sum)
-    v1 = cache_sum(some)
-    v2 = cache_sum(some)
-    v3 = cache_sum(some)
+    v1 = cache_sum([100, 200])
+    v2 = cache_sum([100, 200])
+    v3 = cache_sum([100, 200])
     assert v1 is v2 is v3
 
 
 def test_sum_2():
-    some = 30, 50, 10, 40
     cache_sum = cache(sum)
-    v5 = cache_sum(some)
-    v6 = cache_sum(some)
+    v5 = cache_sum([30, 50, 10, 40])
+    v6 = cache_sum([30, 50, 10, 40])
     assert v5 is v6
