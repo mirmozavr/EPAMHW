ba7e69e724a2240d3fba6dc78c252308239bae84
Mo <mirmozavr@gmail.com>
2021-03-11 22:30:22 +0300

    Pickle import

--- hw2/task04/hw2_task04.py
+++ hw2/task04/hw2_task04.py
@@ -15,6 +15,7 @@ val_2 = cache_func(*some)
 
 assert val_1 is val_2
 """
+import pickle  # noqa: S403
 from typing import Any, Callable
 
 
@@ -30,39 +31,9 @@ def cache(func: Callable) -> Callable:
     f_cache = {}
 
     def wrapper(*args: Any) -> Any:
-        print(args)
-        if isinstance(args, list):
-            args = tuple(*args)
-        if args in f_cache:
-            return f_cache[args]
-
-        f_cache[args] = func(*args)
-        return f_cache[args]
+        pickle_dump = pickle.dumps(args)
+        if pickle_dump not in f_cache:
+            f_cache[pickle_dump] = func(*args)
+        return f_cache[pickle_dump]
 
     return wrapper
-
-
-cache_function = cache(function)
-val_1 = cache_function(*[100, 200])
-val_2 = cache_function(*[100, 200])
-assert val_1 is val_2
-
-
-some = 100, 200
-cache_calc_sum = cache(calc_sum)
-val_3 = cache_calc_sum(*[1, 2])
-val_4 = cache_calc_sum(*[1, 2])
-assert val_3 is val_4
-
-
-cache_sum = cache(sum)
-v1 = cache_sum([100, 200])
-v2 = cache_sum([100, 200])
-v3 = cache_sum([100, 200])
-assert v1 is v2 is v3
-
-
-cache_sum = cache(sum)
-v5 = cache_sum([30, 50, 10, 40])
-v6 = cache_sum([30, 50, 10, 40])
-assert v5 is v6
--- hw2/task04/test_hw2_task04.py
+++ hw2/task04/test_hw2_task04.py
@@ -3,20 +3,19 @@ from hw2_task04 import cache, calc_sum, function
 
 def test_func():
     cache_function = cache(function)
-    val_1 = cache_function(*[100, 200])
-    val_2 = cache_function(*[100, 200])
+    val_1 = cache_function(*(100, 200))
+    val_2 = cache_function(*(100, 200))
     assert val_1 is val_2
 
 
-def test_calc_sum():
-    some = 100, 200
+def test_custom_sum_function():
     cache_calc_sum = cache(calc_sum)
-    val_3 = cache_calc_sum([100, 200])
-    val_4 = cache_calc_sum([100, 200])
+    val_3 = cache_calc_sum(*(22, 44))
+    val_4 = cache_calc_sum(*(22, 44))
     assert val_3 is val_4
 
 
-def test_sum_1():
+def test_sum_list_2_numbers():
     cache_sum = cache(sum)
     v1 = cache_sum([100, 200])
     v2 = cache_sum([100, 200])
@@ -24,8 +23,15 @@ def test_sum_1():
     assert v1 is v2 is v3
 
 
-def test_sum_2():
+def test_sum_list_4_numbers():
     cache_sum = cache(sum)
-    v5 = cache_sum([30, 50, 10, 40])
-    v6 = cache_sum([30, 50, 10, 40])
+    v5 = cache_sum([30, 50, 10, -40])
+    v6 = cache_sum([30, 50, 10, -40])
+    assert v5 is v6
+
+
+def test_sum_list_1_number():
+    cache_sum = cache(sum)
+    v5 = cache_sum([3])
+    v6 = cache_sum([3])
     assert v5 is v6
