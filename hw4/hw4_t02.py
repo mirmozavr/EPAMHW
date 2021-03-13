e5104111c48ab2e43a66e5156154596c4ad10d9a
Mo <mirmozavr@gmail.com>
2021-03-14 01:48:23 +0300

    Hw4 t02

---
+++ hw4/hw4_t02.py
@@ -0,0 +1,47 @@
+"""
+Write a function that accepts an URL as input.
+
+and count how many letters `i` are present in the HTML by this URL.
+
+Write a test that check that your function works.
+Test should use Mock instead of real network interactions.
+
+You can use urlopen* or any other network libraries.
+In case of any network error raise ValueError("Unreachable {url}).
+
+Definition of done:
+ - function is created
+ - function is properly formatted
+ - function has positive and negative tests
+
+You will learn:
+ - how to test using mocks
+ - how to write complex mocks
+ - how to raise an exception form mocks
+ - do a simple network requests
+
+
+>> count_dots_on_i("https://example.com/")
+59
+
+* https://docs.python.org/3/library/urllib.request.html#urllib.request.urlopen
+"""
+from unittest.mock import Mock
+
+import requests
+
+
+def get_url_return_str(url: str) -> str:
+    try:
+        r = requests.get(url)
+    except Exception:
+        raise ValueError(f"Unreachable {url}")
+    return r.text
+
+
+def count_dots_on_i(url: str) -> int:
+    return get_url_return_str(url).count("i")
+
+
+get_url_return_str = Mock()  # noqa: F811
+get_url_return_str.return_value = "22 i symbols in total iiiiiiiiiiiiiiiiiiii"
---
+++ hw4/tests/test_hw4_t02.py
@@ -0,0 +1,5 @@
+from hw4.hw4_t02 import count_dots_on_i
+
+
+def test_count_dots_on_i():
+    assert count_dots_on_i("any.url.com") == 22
