923297d4196fd044e0a30b56bae5a2adc7ecd75f
Mo <mirmozavr@gmail.com>
2021-03-14 23:41:41 +0300

    Review fix, tests refactor

--- hw2/task01/task01.py
+++ hw2/task01/task01.py
@@ -9,13 +9,9 @@ Complete using only default collections:
     5) Find most common non ascii char for document
 """
 
-import sys
+from string import punctuation
 from typing import Generator
 from unicodedata import category
-
-codepoints = range(sys.maxunicode + 1)
-punctuation = {c for i in codepoints if category(c := chr(i)).startswith("P")}
-ASCII = {chr(x) for x in range(256)}
 
 
 def open_file_and_count_chars(file_path: str) -> dict:
@@ -41,7 +37,7 @@ def get_longest_diverse_words(file_path: str) -> list:
     top_10_words = [["", 0] for _ in range(10)]
     for line in open_file_and_yield_line(file_path):
         for word in line.split():
-            word = word.strip("".join(punctuation))
+            word = word.strip(punctuation)
             unique_length = len(set(word))
             for i in range(10):
                 if unique_length > top_10_words[i][1]:
@@ -66,7 +62,7 @@ def count_punctuation_chars(file_path: str) -> int:
     punctuation_char_count = 0
     counter = open_file_and_count_chars(file_path)
     for char in counter:
-        if char in punctuation:
+        if category(char).startswith("P"):
             punctuation_char_count += counter[char]
     return punctuation_char_count
 
@@ -75,7 +71,7 @@ def count_non_ascii_chars(file_path: str) -> int:
     non_ascii_char_count = 0
     counter = open_file_and_count_chars(file_path)
     for char in counter:
-        if char not in ASCII:
+        if not char.isascii():
             non_ascii_char_count += counter[char]
     return non_ascii_char_count
 
@@ -85,6 +81,6 @@ def get_most_common_non_ascii_char(file_path: str) -> str:
     candidate_char = ""
     candidate_char_count = -float("INF")
     for char in counter:
-        if char not in ASCII and counter[char] > candidate_char_count:
+        if not char.isascii() and counter[char] > candidate_char_count:
             candidate_char, candidate_char_count = char, counter[char]
     return candidate_char  # noqa: R504
