9c9ba816ee47953027fc5b3c61877e105bc69fb0
Mo <mirmozavr@gmail.com>
2021-03-15 21:58:34 +0300

    Test rename

--- hw4/tests/test_hw4_t03.py
+++ hw4/tests/test_hw4_t03.py
@@ -1,21 +1,21 @@
 from hw4.hw4_t03 import my_precious_logger
 
 
-def test_stdout_simple_text(capsys):
+def test_text_to_stdout_without_error(capsys):
     my_precious_logger("basically, this is any text")
     out, err = capsys.readouterr()
     assert out == "basically, this is any text\n"
     assert err == ""
 
 
-def test_stdout_mixed_text(capsys):
+def test_text_to_stdout_with_error_word_in_the_middle(capsys):
     my_precious_logger("please raise error now")
     out, err = capsys.readouterr()
     assert out == "please raise error now\n"
     assert err == ""
 
 
-def test_stderr_mixed_text(capsys):
+def test_text_to_stderr_without_stdout_text(capsys):
     my_precious_logger("error: some text")
     out, err = capsys.readouterr()
     assert out == ""
