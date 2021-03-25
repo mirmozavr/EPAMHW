1a6f363b4f886317c8eb67da4fe91636ed4595d6
Movses <68658828+mirmozavr@users.noreply.github.com>
2021-03-25 17:38:12 +0300

    Hw5 t02 (#21)
    * Hw5 t02 decorator
    
    * Think first, code second
    
    * Refactor
    
    * Remove calculation from assert

---
+++ hw5/tests/test_hw5_t02.py
@@ -0,0 +1,30 @@
+import functools
+from unittest.mock import Mock
+
+from hw5.hw5_t02 import print_result
+
+
+@print_result
+def custom_sum(*args):
+    """This function can sum any objects which have __add___."""  # noqa: D401
+    return functools.reduce(lambda x, y: x + y, args)
+
+
+def test_tuple_of_integers():
+    assert custom_sum(1, 2, 3, 4) == 10
+
+
+def test_three_lists_with_integers():
+    assert custom_sum([1, 2, 3], [9, 9], [4, 5]) == [1, 2, 3, 9, 9, 4, 5]
+
+
+def test_text_arguments():
+    assert custom_sum("dragon", "fly") == "dragonfly"
+
+
+def test_saved_function_is_original_function():
+    func = Mock(__name__="function name", __doc__="function docstring")
+    custom_func = print_result(func)
+    assert custom_func.__original_func is func
+    assert custom_func.__name__ == "function name"
+    assert custom_func.__doc__ == "function docstring"
