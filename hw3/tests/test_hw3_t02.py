eeb7b99a460a32601ea608e0886c873b61fded14
Mo <mirmozavr@gmail.com>
2021-03-29 23:49:27 +0300

    Pytest.mark.skip

--- hw3/tests/test_hw3_t02.py
+++ hw3/tests/test_hw3_t02.py
@@ -1,5 +1,8 @@
-"""from hw3.hw3_t02 import slow_calc_500_numbers.
+import pytest
 
+from hw3.hw3_t02 import slow_calc_500_numbers
+
+
+@pytest.mark.skip(reason="test takes too much time")
 def test_slow_calc_500_numbers():
     assert sum(slow_calc_500_numbers()) == 1025932
-"""
