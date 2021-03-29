b2e3df1183bdff3329aec635f195cc86cb0fd3c9
Mo <mirmozavr@gmail.com>
2021-03-29 19:10:38 +0300

    Easy char count

--- hw7/hw7_t01.py
+++ hw7/hw7_t01.py
@@ -10,7 +10,9 @@ Tree can only contains basic structures like:
 from typing import Any, Union
 
 
-def parse_collection(parsed_collection: Union[list, set, tuple], element: Any) -> int:
+def parse_collection(
+    parsed_collection: Union[list, set, tuple, str], element: Any
+) -> int:
     counter = 0
     for value in parsed_collection:
         if value == element:
@@ -20,15 +22,7 @@ def parse_collection(parsed_collection: Union[list, set, tuple], element: Any) -
         elif isinstance(value, (list, set, tuple)):
             counter += parse_collection(value, element)
         elif isinstance(element, str) and len(element) == 1 and isinstance(value, str):
-            counter += parse_string(value, element)
-    return counter
-
-
-def parse_string(parsed_string: str, element: str) -> int:
-    counter = 0
-    for char in parsed_string:
-        if char == element:
-            counter += 1
+            counter += value.count(element)
     return counter
 
 
@@ -41,5 +35,4 @@ def find_occurrences(tree: dict, element: Any) -> int:
             counter += find_occurrences(value, element)
         elif isinstance(value, (list, set, tuple, str)):
             counter += parse_collection(value, element)
-
     return counter
