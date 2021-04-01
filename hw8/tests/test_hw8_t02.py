30fa16aff1f67cab3b9a2f8e71708ec7d5d1616d
Mo <mirmozavr@gmail.com>
2021-04-01 23:17:19 +0300

    Add iteration test

--- hw8/tests/test_hw8_t02.py
+++ hw8/tests/test_hw8_t02.py
@@ -19,3 +19,15 @@ def test_table_data_contains():
 def test_table_data_get_item():
     with TableData(db_path, "presidents") as presidents:
         assert presidents["Trump"] == ("Trump", 1337, "US")
+
+
+def test_table_data_is_iterable_by_rows():
+    with TableData(db_path, "presidents") as presidents:
+        names, ages, countries = [], [], []
+        for president in presidents:
+            names.append(president["name"])
+            ages.append(president["age"])
+            countries.append(president["country"])
+        assert names == ["Yeltsin", "Trump", "Big Man Tyrone"]
+        assert ages == [999, 1337, 101]
+        assert countries == ["Russia", "US", "Kekistan"]
