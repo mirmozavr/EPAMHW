23ae16124c520d8a9c5cfc8539f7291980cbb6c1
Mo <mirmozavr@gmail.com>
2021-04-02 00:32:20 +0300

    Complex test, file rename

--- hw9/tests/test_hw9_t01.py
+++ hw9/tests/test_hw9_t01.py
@@ -5,15 +5,17 @@ from hw9.hw9_t01 import merge_sorted_files
 file_dir = os.path.dirname(__file__)
 
 
-def test_merge_4_sorted_files():
+def test_merge_sorted_files():
     assert (
-        merge_sorted_files(
-            [
-                file_dir + "/file1.txt",
-                file_dir + "/file2.txt",
-                file_dir + "/file3.txt",
-                file_dir + "/file4.txt",
-            ]
+        list(
+            merge_sorted_files(
+                [
+                    file_dir + "/positive_nums_1.txt",
+                    file_dir + "/positive_nums_2.txt",
+                    file_dir + "/negative_nums.txt",
+                    file_dir + "/empty_file.txt",
+                ]
+            )
         )
-        == [1, 2, 3, 4, 5]
+        == [-4, 1, 2, 3, 3, 3, 4, 5, 6, 7, 9]
     )
