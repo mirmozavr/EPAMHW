f8f92b017a141f532c5089fdcd5e56b59da3d37a
Mo <mirmozavr@gmail.com>
2021-04-03 20:52:33 +0300

    Hw9 t03

---
+++ hw9/hw9_t03.py
@@ -0,0 +1,47 @@
+"""
+Write a function that takes directory path, a file extension and an optional tokenizer.
+
+It will count lines in all files with that extension if there are no tokenizer.
+If a the tokenizer is not none, it will count tokens.
+For dir with two files from hw1.py:
+universal_file_counter(test_dir, "txt")
+6
+universal_file_counter(test_dir, "txt", str.split)
+6
+"""
+import os
+from pathlib import Path
+from typing import Callable, Optional, Union
+
+
+def count_lines(file_path: Union[str, Path]) -> int:
+    line_counter = 0
+    with open(file_path) as file:
+        for _line in file:
+            line_counter += 1
+    return line_counter
+
+
+def count_tokens(file_path: Union[str, Path], tokenizer: Callable) -> int:
+    token_counter = 0
+    with open(file_path) as file:
+        for line in file:
+            for _item in tokenizer(line):
+                token_counter += 1
+    return token_counter
+
+
+def universal_file_counter(
+    dir_path: Path, file_extension: str, tokenizer: Optional[Callable] = None
+) -> int:
+    counter = 0
+    file_list = [
+        name for name in os.listdir(dir_path) if name.endswith("." + file_extension)
+    ]
+
+    for name in file_list:
+        if tokenizer is None:
+            counter += count_lines("/".join((dir_path, name)))
+        else:
+            counter += count_tokens("/".join((dir_path, name)), tokenizer)
+    return counter
---
+++ hw9/tests/test_hw9_t03.py
@@ -0,0 +1,25 @@
+import os
+
+from hw9.hw9_t03 import universal_file_counter
+
+base_dir = os.path.dirname(__file__)
+
+
+def test_count_lines_in_txt():
+    assert universal_file_counter(base_dir, "txt") == 7
+
+
+def test_count_words_in_txt():
+    assert universal_file_counter(base_dir, "txt", str.split) == 17
+
+
+def test_count_lines_in_py():
+    assert universal_file_counter(base_dir, "py") == 24
+
+
+def test_count_words_in_rst():
+    assert universal_file_counter(base_dir, "rst", str.split) == 7
+
+
+def test_count_lines_in_jpg():
+    assert universal_file_counter(base_dir, "jpg") == 0
