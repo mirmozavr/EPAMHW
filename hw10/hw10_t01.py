a9e31d1349aca9e64b0899b44184944f0f57e5a3
Mo <mirmozavr@gmail.com>
2021-04-09 02:39:55 +0300

    All parsing done

--- hw10/hw10_t01.py
+++ hw10/hw10_t01.py
@@ -37,6 +37,8 @@ import aiohttp
 import asyncio
 import datetime as dt
 
+sp_500_info = []
+
 
 async def get_usd_currency_from_cbr(session):
     today_date = dt.datetime.strftime(dt.datetime.today(), "%d/%m/%Y")
@@ -49,7 +51,7 @@ async def get_usd_currency_from_cbr(session):
         "http://www.cbr.ru/scripts/XML_dynamic.asp", params=usd_params
     ) as resp:
         html = await resp.text()
-        soup = BS(html)
+        soup = BS(html, features="html.parser")
         USD = float(soup.find("value").string.replace(",", "."))
         print("Dollar", USD)
         return USD
@@ -59,82 +61,90 @@ async def get_company_info(url, session, USD):
     async with session.get(
         url
     ) as resp:
-        print("Status:", resp.status)
-        print("Content-type:", resp.headers["content-type"])
-        print(resp.url)
+        # print("Status:", resp.status)
+        # print("Content-type:", resp.headers["content-type"])
+        # print(resp.url)
         html = await resp.text()
-        soup = BS(html)
+        soup = BS(html, features="html.parser")
         label = str(soup.find("span", attrs={"class": "price-section__label"}).string)
         current_value = float(
-            soup.find("span", attrs={"class": "price-section__current-value"}).string
+            soup.find("span", attrs={"class": "price-section__current-value"}).string.replace(",", "")
         )
         code = list(
             soup.find("span", attrs={"class": "price-section__category"}).strings
         )[-2].strip(", ")
-        p_e = float(
-            list(
-                soup.find(
-                    "div", attrs={"class": "snapshot__header"}, string="P/E Ratio"
-                ).parent.stripped_strings
-            )[0]
-        )
-        year_low = float(
-            list(
-                soup.find(
-                    "div", attrs={"class": "snapshot__header"}, string="52 Week Low"
-                ).parent.stripped_strings
-            )[0]
-        )
-        year_high = float(
-            list(
-                soup.find(
-                    "div", attrs={"class": "snapshot__header"}, string="52 Week High"
-                ).parent.stripped_strings
-            )[0]
-        )
-        potential_profit = round((year_high - year_low) * USD,2)
-        print(potential_profit)
+        try:
+            p_e = float(
+                list(
+                    soup.find(
+                        "div", attrs={"class": "snapshot__header"}, string="P/E Ratio"
+                    ).parent.stripped_strings
+                )[0].replace(",", "")
+            )
+        except AttributeError:
+            p_e = None
+
+        try:
+            year_low = float(
+                list(
+                    soup.find(
+                        "div", attrs={"class": "snapshot__header"}, string="52 Week Low"
+                    ).parent.stripped_strings
+                )[0].replace(",", "")
+            )
+            year_high = float(
+                list(
+                    soup.find(
+                        "div", attrs={"class": "snapshot__header"}, string="52 Week High"
+                    ).parent.stripped_strings
+                )[0].replace(",", "")
+            )
+            potential_profit = round((year_high - year_low) * USD, 2)
+        except AttributeError:
+            potential_profit = None
 
         return code, current_value, p_e, potential_profit
 
 
-async def get_top_500_info():
+async def get_sp_500_info():
+    global sp_500_info
     base_url = "https://markets.businessinsider.com/index/components/s&p_500"
     base_company_url = "https://markets.businessinsider.com"
-    top_500_info = []
     async with aiohttp.ClientSession() as session:
 
         USD = await get_usd_currency_from_cbr(session)
 
-        for page in range(1, 2):  # extend to 12!!!
+        for page in range(6, 7):  # extend to 12!!!
             async with session.get(base_url, params=[("p", page)]) as resp:
                 only_a_tags = SoupStrainer("a")
 
-                print("Status:", resp.status)
-                print(resp.url)
+                # print("Status:", resp.status)
+                # print(resp.url)
                 html = await resp.text()
 
-                soup = BS(html, parse_only=only_a_tags)
+                soup = BS(html, features="html.parser")
 
                 stocks = soup.find_all(
                     "a", span="", href=re.compile("stock$"), attrs={"title": True}
                 )
                 print(len(stocks), "len stocks")
 
-                company_info = {
-                    "name": None,
-                    "code": None,
-                    "price": None,
-                    "P/E": None,
-                    "growth": None,
-                    "potential profit": None,
-                }
-                for item in stocks:
-                    print(item["href"], item["title"])
+                for company in stocks:
+                    company_info = {
+                        "name": None,
+                        "code": None,
+                        "price": None,
+                        "P/E": None,
+                        "growth": None,
+                        "potential profit": None,
+                    }
+                    # print(item["href"], item["title"])
+                    growth = float(list(list(company.parent.next_siblings)[-4].children)[1].string.replace(",", ""))
 
-                    company_info["name"] = item["title"]
+                    company_info["name"] = company["title"]
+                    company_info["growth"] = growth
 
-                    URL = base_company_url + item["href"]
+                    URL = base_company_url + company["href"]
 
                     (
                         company_info["code"],
@@ -144,8 +154,11 @@ async def get_top_500_info():
                     ) = (await get_company_info(URL, session, USD))
 
                     print(company_info)
-                    top_500_info.append(company_info)
+                    sp_500_info.append(company_info)
 
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(get_top_500_info())
+loop.run_until_complete(get_sp_500_info())
+print("500 len", len(sp_500_info))
+for info in sp_500_info:
+    print(info)
