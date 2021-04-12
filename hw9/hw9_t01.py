c7063b7ae0fbe5c308d6bb1472b86e87fd9face4
Mo <mirmozavr@gmail.com>
2021-04-13 02:42:52 +0300

    Decomposition and single digit bubble

--- hw9/hw9_t01.py
+++ hw9/hw9_t01.py
@@ -16,27 +16,30 @@ from pathlib import Path
 from typing import Iterator, List, Union
 
 
+def merge_sorted_files(file_list: List[Union[Path, str]]) -> Iterator:
+    queue = list(map(open_file_return_iterator, file_list))
+    result = []
+    while queue:
+        for file in queue:
+            load_next_or_remove_generator(file, queue, result)
+            bubble_single_digit(result)
+    return iter(result)
+
+
 def open_file_return_iterator(path: Union[Path, str]) -> Iterator:
     with open(path) as file:
         yield from (int(row) for row in file)
 
 
-def load_next_or_remove_generator(queue: list, result: list) -> None:
-    for file in queue:
-        try:
-            result.append(next(file))
-        except StopIteration:
-            queue.remove(file)
+def load_next_or_remove_generator(file: Iterator, queue: list, result: list) -> None:
+    try:
+        result.append(next(file))
+    except StopIteration:
+        queue.remove(file)
 
 
-def merge_sorted_files(file_list: List[Union[Path, str]]) -> Iterator:
-    queue = list(map(open_file_return_iterator, file_list))
-    result = []
-    while queue:
-        load_next_or_remove_generator(queue, result)
-
-        for i in range(len(result) - len(queue), len(result)):
-            while i > 0 and result[i] < result[i - 1]:
-                result[i], result[i - 1] = result[i - 1], result[i]
-                i -= 1
-    return iter(result)
+def bubble_single_digit(result: list) -> None:
+    i = len(result) - 1
+    while i > 0 and result[i] < result[i - 1]:
+        result[i], result[i - 1] = result[i - 1], result[i]
+        i -= 1
