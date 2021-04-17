15f0d03496d6ee7a26af26476357eb093bb56d2c
Mo <mirmozavr@gmail.com>
2021-04-17 18:31:17 +0300

    Hw11 t01

--- hw11/hw11_t01.py
+++ hw11/hw11_t01.py
@@ -22,3 +22,18 @@ class SizesEnum(metaclass=SimplifiedEnum):
 assert ColorsEnum.RED == "RED"
 assert SizesEnum.XL == "XL"
 """
+
+
+class UpperAttrMetaclass(type):
+
+    def __new__(cls, clsname, bases, dct):
+
+        uppercase_attr = {}
+        for name, val in dct.items():
+            if not name.startswith('__'):
+                uppercase_attr[name.upper()] = val
+            else:
+                uppercase_attr[name] = val
+
+        return type.__new__(cls, clsname, bases, uppercase_attr)
+
