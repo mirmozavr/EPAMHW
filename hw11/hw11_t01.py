d6965e70a91367dd0946afa6ec93a7158f477081
Movses <68658828+mirmozavr@users.noreply.github.com>
2021-04-28 20:51:32 +0300

    Hw11 t01 (#34)
    * Hw11 t01
    
    * Hw11 t01
    
    * Hw11 t01
    
    * Minor fix
    
    * Tests

---
+++ hw11/hw11_t01.py
@@ -0,0 +1,32 @@
+"""
+Vasya implemented nonoptimal Enum classes.
+
+Remove duplications in variables declarations using metaclasses.
+from enum import Enum
+class ColorsEnum(Enum):
+    RED = "RED"
+    BLUE = "BLUE"
+    ORANGE = "ORANGE"
+    BLACK = "BLACK"
+class SizesEnum(Enum):
+    XL = "XL"
+    L = "L"
+    M = "M"
+    S = "S"
+    XS = "XS"
+Should become:
+class ColorsEnum(metaclass=SimplifiedEnum):
+    __keys = ("RED", "BLUE", "ORANGE", "BLACK")
+class SizesEnum(metaclass=SimplifiedEnum):
+    __keys = ("XL", "L", "M", "S", "XS")
+assert ColorsEnum.RED == "RED"
+assert SizesEnum.XL == "XL"
+"""
+
+
+class SimplifiedEnum(type):
+    def __new__(cls, clsname: str, bases: type, dct: dict) -> type:
+        for name in dct[f"_{clsname}__keys"]:
+            cls.__setattr__(cls, name, name)
+
+        return type.__new__(cls, clsname, bases, dct)
